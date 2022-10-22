a79568c54b7f37459618b6179aab0c0a19dd3b42
jatin510 <jatin6972@gmail.com>
2022-10-22 11:55:56 +0530

    added shared domain

---
+++ src/shared/domain/uniqueEntityID.ts
@@ -0,0 +1,8 @@
+import { Identifier } from './identifier';
+import { randomUUID } from 'crypto';
+
+export class UniqueEntityID extends Identifier<string | number> {
+  constructor(id?: string | number) {
+    super(id ? id : randomUUID());
+  }
+}
