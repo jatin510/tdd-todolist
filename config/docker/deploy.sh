567b2babfedbe869692a5a658b5850e7854a4179
jatin510 <jatin6972@gmail.com>
2022-10-29 19:13:57 +0530

    updated deploy.sh

--- config/docker/deploy.sh
+++ config/docker/deploy.sh
@@ -2,7 +2,7 @@ echo 'Starting to Deploy...'
 ssh root@194.195.113.217 "
 	sudo docker info
 	cd /home/ubuntu/todolist-server/config/docker
-	sudo docker pull docker pull jatin510/todo-list-server:latest
+	sudo docker pull jatin510/todo-list-server:latest
    sudo docker-compose down
    sudo docker-compose -f docker-compose.dev.yml up -d
    "
