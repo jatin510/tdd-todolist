8a40b35787814755f64f831ac68b163a6ff5af1c
jatin510 <jatin6972@gmail.com>
2022-10-29 19:17:27 +0530

    updated docker compose command

--- config/docker/deploy.sh
+++ config/docker/deploy.sh
@@ -3,7 +3,7 @@ ssh root@194.195.113.217 "
 	sudo docker info
 	cd /home/ubuntu/todolist-server/config/docker
 	sudo docker pull jatin510/todo-list-server:latest
-   sudo docker-compose down
-   sudo docker-compose -f docker-compose.dev.yml up -d
+   sudo docker compose down
+   sudo docker compose -f docker-compose.yml up -d
    "
 echo 'Deployment completed successfully'
